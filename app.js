6568348f2a04589d7ef0aa2c25d59ee216a71c3a
tutu <1518474913@qq.com>
2018-01-03 15:39:28 +0800

    branch test

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
 
-// app有3个生命周期函数
-// App({
-//   onLaunch:function(){
-//     console.log("onLaunch");   
-//   },
-//   onShow:function(){
-//     console.log("onShow");
-//   },
-//   onHide:function(){
-//     console.log('onHide')
-//   }
-// })
-
-
-// 在页面page里面实现的是页面周期的控制，而在APP里则是关于应用程序
-// 生命周期的控制:globalData专门用于保存我们用于全局的数据
-// g_currentMusicPostId指代的是哪一个音乐在播放
 App({
     globalData:{
        g_isPlayingMusic:false,
